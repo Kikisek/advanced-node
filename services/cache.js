c792975e22597151e02f2d7a2590334b86887e15
Kristyna Svecova <kristyna@rewardops.com>
2020-11-06 06:15:42 -0800

    Create unique key for caching queries

--- services/cache.js
+++ services/cache.js
@@ -3,5 +3,9 @@ const mongoose = require('mongoose');
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function () {
+  const key = Object.assign({}, this.getQuery(), {
+    collection: this.mongooseCollection.name,
+  });
+
   return exec.apply(this, arguments);
 };
