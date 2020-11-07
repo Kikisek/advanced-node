1e6e1a16f6391ba756574f04bf110b5b7321f67c
Kristyna Svecova <kristyna@rewardops.com>
2020-11-06 16:19:58 -0800

    Implement cache

--- services/cache.js
+++ services/cache.js
@@ -8,10 +8,29 @@ client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-  const key = Object.assign({}, this.getQuery(), {
-    collection: this.mongooseCollection.name,
-  });
+mongoose.Query.prototype.exec = async function () {
+  const key = JSON.stringify(
+    Object.assign({}, this.getQuery(), {
+      collection: this.mongooseCollection.name,
+    })
+  );
 
-  return exec.apply(this, arguments);
+  // See if we have a value for 'key' in redis
+  const cacheValue = await client.get(key);
+
+  // If we do, return that
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
+
+    return Array.isArray(doc)
+      ? doc.map((d) => new this.model(d))
+      : new this.model(doc);
+  }
+
+  // Otherwise, issue the queru and store the result in redis
+  const result = await exec.apply(this, arguments);
+
+  client.set(key, JSON.stringify(result));
+
+  return result;
 };
