28f01802957ccbbe3a98bec2989682d6805d49c5
Kristyna Svecova <kristyna@rewardops.com>
2020-12-08 11:12:45 -0800

    Add test to check redirection during login

--- test/header.test.js
+++ test/header.test.js
@@ -14,8 +14,15 @@ afterEach(async () => {
   await browser.close();
 });
 
-test('Header has correct text', async () => {
+test('the header has correct text', async () => {
   const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
 
   expect(text).toEqual('Blogster');
+});
+
+test('clicking Login starts oauth flow', async () => {
+  await page.click('.right a');
+
+  const url = await page.url();
+  expect(url).toMatch(/accounts\.google\.com/);
 });
