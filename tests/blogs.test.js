7c6ea1966ebffa56d7fa3dcd0ff4abb1b057fd5a
Kristyna Svecova <kristyna@rewardops.com>
2021-01-13 11:16:45 -0800

    Test blog creation

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -21,4 +21,41 @@ describe('When logged in', async () => {
     const label = await page.getContentsOf('form label');
     expect(label).toEqual('Blog Title');
   });
+
+  describe('And using invalid inputs', async () => {
+    beforeEach(async () => {
+      await page.click('form button');
+    });
+
+    test('the form shows an error message', async () => {
+      const titleError = await page.getContentsOf('.title .red-text');
+      const contentError = await page.getContentsOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
+  });
+
+  describe('And using valid inputs', async () => {
+    beforeEach(async () => {
+      await page.type('.title input', 'Title test');
+      await page.type('.content input', 'Content test');
+      await page.click('form button');
+    });
+    test('submitting takes use to review screen', async () => {
+      const text = await page.getContentsOf('h5');
+      expect(text).toEqual('Please confirm your entries');
+    });
+
+    test('submitting then saving adds blog to index page', async () => {
+      await page.click('button.green');
+      await page.waitFor('.card');
+
+      const title = await page.getContentsOf('.card-title');
+      const content = await page.getContentsOf('p');
+
+      expect(title).toEqual('Title test');
+      expect(content).toEqual('Content test');
+    });
+  });
 });
