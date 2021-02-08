182d17434a918a9728307ad0b5fe0d5710c32115
Kristyna Svecova <kristyna@rewardops.com>
2021-02-08 13:44:48 -0800

    Update ci code

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '989667732545-49ro7ogd674p9vuvqts0ulsol47ds3kl.apps.googleusercontent.com',
+  googleClientSecret: 'RDQ5iRPunCVPdz76p5emBIwF',
+  mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379',
+};
