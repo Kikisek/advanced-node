d16372843bf83a1fbbd152453882c49b88e70501
Kristyna Svecova <kristyna@rewardops.com>
2021-02-16 15:34:39 -0800

    Create route for uploading images

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    '989667732545-49ro7ogd674p9vuvqts0ulsol47ds3kl.apps.googleusercontent.com',
-  googleClientSecret: 'RDQ5iRPunCVPdz76p5emBIwF',
-  mongoURI:
-    'mongodb://node:UctkPQs9JfmJaCNM@cluster0-shard-00-00.gugmg.mongodb.net:27017,cluster0-shard-00-01.gugmg.mongodb.net:27017,cluster0-shard-00-02.gugmg.mongodb.net:27017/AdvancedNode?ssl=true&replicaSet=atlas-148cz8-shard-0&authSource=admin&retryWrites=true&w=majority',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379',
-};
